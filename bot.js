48a5e41d7205d4cb37e30aad91d0417990ce23a5
xMasTeR <43861336+iiMostafaYT@users.noreply.github.com>
2018-12-24 17:17:49 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -826,7 +826,7 @@ client.on("guildMemberAdd", (member) => {
             if (dat[Inv])
                 if (dat[Inv] < Invite.uses) {
                     console.log(3);
- channel.send(`**Welcome To zCodes** :gem:${member} n/ Invited By ${Invite.inviter}`)
+ channel.send(`**Welcome To zCodes** :gem: \n ${member} Invited By ${Invite.inviter} \n Have a Nice Time 🎶`)
  }
             dat[Inv] = Invite.uses;
         })
